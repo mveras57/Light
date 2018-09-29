9fb7b33ac92c732cb9f1c63ec23460522f46eec3
Marco AV Silva <mavs57@me.com>
2018-09-29 17:21:50 -0300

    First App build from Apple's book App Development with Swift.

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
 
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
 }
 
