d650f064a932c1d0917d12666326c5129dd0e98d
Marco AV Silva <mavs57@me.com>
2018-09-29 13:33:16 -0300

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Marco AV Silva on 29/09/18.
+//  Copyright © 2018 Marco AV Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
